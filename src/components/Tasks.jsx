75b09a94c27fad535abb7dea01f5aa08659404e7
André Oliveira <andre_oliveira809@hotmail.com>
2022-09-26 22:24:03 +0100

    improved task UI on smaller devices

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -33,12 +33,12 @@ const Tasks = () => {
 			<Navbar />
 			<div className="flex">
 				<Sidebar />
-				<section className="top-20 bg-light-gray w-screen h-screen px-12 flex flex-col justify-center items-center">
+				<section className="top-20 bg-light-gray w-screen h-screen px-8 sm:px-12 flex flex-col justify-center items-center">
 					<h1 className="text-black origin-left font-medium text-2xl text-center mt-8 mb-6">
 						Task List
 					</h1>
 					<div className="w-full h-full overflow-y-scroll scroll scrollbar-hide">
-						<div className="px-10 py-2 inline-block rounded-lg h-screen bg-light-gray w-full">
+						<div className="p-2 sm:px-10 inline-block rounded-lg h-screen bg-light-gray w-full">
 							{tasks.map(task => {
 								return <Task key={task.id} {...task} />;
 							})}
